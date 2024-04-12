7096a4a6cffb36c693390c83734a7b6b4bd1baa0
Andreas Buer Vase <andreas.vase@gmail.com>
2024-04-12 10:34:25 -0400

    Added a new branch for HU01

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from adduser import *
+
 # Import necessary modules or packages
 
 # Define any global variables or constants
 
 # Define any helper functions or classes
 
+
 # Define the main function
 def main():
-    print("Hello, World!")
+    create_profile()
     # Your code goes here
+
 
 # Check if the file is being run as the main program
 if __name__ == "__main__":
