ca8dcda31909d2d9b37a613119aefe760f5b84ee
rcuello <raul.cuello@sansano.usm.cl>
2024-04-12 22:36:23 -0400

    mejora

--- common.py
+++ common.py
@@ -5,6 +5,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 import os
 
+from openpyxl import load_workbook
+
 def check_username(username):
     wb = load_workbook('users_data.xlsx')
     hoja = wb.worksheets[0]
@@ -16,22 +18,36 @@ def check_username(username):
     return repetido
 
 def check_password(password, confirmpassword):
-        if len(password) < 8:
-            print("Password must be at least 8 characters long.")
+    if len(password) < 8:
+        print("Password must be at least 8 characters long.")
+        return False
+    
+    # Check if password and confirm password match
+    if password != confirmpassword:
+        print("Passwords do not match. Please try again.")
+        return False
+    
+    # Check if password contains only allowed characters
+    allowed_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz°.!\"#$%&/()<=¿?¡{}*_+-[].@[^;"
+    for char in password:
+        if char not in allowed_chars:
+            print("Password contains invalid characters. Please try again.")
             return False
-        
-        # Check if password and confirm password match
-        if password != confirmpassword:
-            print("Passwords do not match. Please try again.")
-            return False
-        
-        # Check if password contains only allowed characters
-        allowed_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz°.!\"#$%&/()<=¿?¡{}*_+-[].@[^;"
-        for char in password:
-            if char not in allowed_chars:
-                print("Password contains invalid characters. Please try again.")
-                return False
-        return True
+    return True
+
+def confirm_password(username, password):
+    wb = load_workbook('users_data.xlsx')
+    hoja = wb.worksheets[0]
+    for fila in hoja.iter_rows(min_row = 2):
+        val_row = [celda.value for celda in fila]
+        if val_row[0] == username :
+            user_db = wb.worksheets[int(val_row[2])]
+            for row in user_db.iter_rows(min_row=2, max_row=2):
+                val_fila = [celda.value for celda in row]
+                if password == val_fila[1]: 
+                    return True
+    print("Contraseña errónea!\n")
+    return False
 
 def generate_password():
     # Define the default characters
